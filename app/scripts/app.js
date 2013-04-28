f1146744e6b50d1e01098d67e0e2a39a535c8b0e
Jørgen Hookham <JorgenHookham@Gmail.com>
2013-04-28 15:30:37 -0700

    Add prioritization and different content types.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,14 +1,3 @@
 'use strict';
 
 var saveINTEapp = angular.module('saveINTEapp', []);
-
-saveINTEapp.config(function ($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'views/main.html',
-      controller: 'MainCtrl'
-    })
-    .otherwise({
-      redirectTo: '/'
-    });
-});
--- app/scripts/controllers/heroGrid.js
+++ app/scripts/controllers/heroGrid.js
@@ -3,24 +3,34 @@
 saveINTEapp.controller('heroGridController',
     function heroGridController ($scope) {
 
-        $scope.heroBoxes = [
+        $scope.photoBoxes = [
             {
                 title: 'Save The Future',
                 caption: 'The IDL program is too awesome.',
                 imageUrl: 'save-the-future.jpg',
-                type: 'photo'
+                type: 'photo',
+                priority: 2
             },
             {
                 title: 'Back At The Lab',
                 caption: 'A Weekend at the familiar AR209 to save the program that helped make us.',
                 imageUrl: 'back-at-the-lab.jpg',
-                type: 'photo'
-            },
+                type: 'photo',
+                priority: 1
+            }
+        ];
+
+        $scope.textBoxes = [
+
             {
                 title: 'All The Channels',
                 caption: 'We push every one of them.',
-                type: 'text'
+                type: 'text',
+                priority: 1
             }
         ];
+
+        var heroBoxes = [].concat($scope.textBoxes).concat($scope.photoBoxes);
+        $scope.heroBoxes = heroBoxes.sort(function(a,b) { return parseFloat(a.priority) - parseFloat(b.priority) } );
 
 });
