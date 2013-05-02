12dbcd00c6e474771d1b36c1932f73ff6481b4a5
Jørgen Hookham <JorgenHookham@Gmail.com>
2013-05-01 22:14:55 -0700

    Add content.

--- app/scripts/controllers/heroGrid.js
+++ app/scripts/controllers/heroGrid.js
@@ -20,58 +20,71 @@ saveINTEapp.controller('heroGridController',
 
         // Hero Grid Items (photos)
 
-        $scope.photoBoxes = [
-            {
-                title: 'Save The Future',
-                caption: 'The IDL program is too awesome.',
-                imageUrl: 'save-the-future.jpg',
-                type: 'photo',
-                priority: 2,
-                template: 'hero-box-templates/photo.html'
-            },
-            {
-                title: 'Back At The Lab',
-                caption: 'A Weekend at the familiar AR209 to save the program that helped make us.',
-                imageUrl: 'back-at-the-lab.jpg',
-                type: 'photo',
-                priority: 2,
-                template: 'hero-box-templates/photo.html'
-            }
-        ];
+        // $scope.photoBoxes = [
+        //     {
+        //         title: 'Save The Future',
+        //         caption: 'The IDL program is too awesome.',
+        //         imageUrl: 'save-the-future.jpg',
+        //         type: 'photo',
+        //         priority: 2,
+        //         template: 'hero-box-templates/photo.html'
+        //     },
+        //     {
+        //         title: 'Back At The Lab',
+        //         caption: 'A Weekend at the familiar AR209 to save the program that helped make us.',
+        //         imageUrl: 'back-at-the-lab.jpg',
+        //         type: 'photo',
+        //         priority: 2,
+        //         template: 'hero-box-templates/photo.html'
+        //     }
+        // ];
 
         // Hero Grid Items (quote)
 
         $scope.quoteBoxes = [
             {
-                quote: 'All The Channels',
-                author: 'We push every one of them.',
-                credential: 'Huge Bigshot',
+                quote: 'The goal is to put people in the work force and be employable, we look to these programs for our new stars.',
+                author: 'Jason Bailey',
+                credential: 'CEO, East Side Games',
+                imageUrl: 'people/jason-bailey.jpg',
                 type: 'quote',
-                priority: 4,
+                priority: 2,
                 template: 'hero-box-templates/quote.html'
             },
             {
-                quote: 'Back to the lab!',
-                author: 'Saturday morning cartoons!',
-                credential: 'Huge Bigshot',
-                type: 'quote',
-                priority: 4,
-                template: 'hero-box-templates/quote.html'
-            },
-            {
-                quote: 'An Excess of Support',
-                author: 'Almost out of hand.',
-                credential: 'Huge Bigshot',
+                quote: 'We have one of the best programs in the university, if anything it should be looked at as one of it\'s crowned jewels.',
+                author: 'Parker Busswood',
+                credential: 'Front-End Engineer, East Side Games',
+                imageUrl: 'people/parker-busswood.jpg',
                 type: 'quote',
                 priority: 1,
                 template: 'hero-box-templates/quote.html'
             },
             {
-                quote: 'Top Dog',
-                author: 'High priority content.',
-                credential: 'Huge Bigshot',
+                quote: 'People from the INTE program come ready to wear many hats. Cutting this program would leave a big gap.',
+                author: 'Josh Nilson',
+                credential: 'COO, East Side Games',
+                imageUrl: 'people/josh-nilson.jpg',
                 type: 'quote',
-                priority: 1,
+                priority: 4,
+                template: 'hero-box-templates/quote.html'
+            },
+            {
+                quote: 'As an alumni I stand to lose my biggest network, fresh talent coming out of the program.',
+                author: 'Jørgen Hookham',
+                credential: 'Designer, Sokanu',
+                imageUrl: 'people/jorgen-hookham.jpg',
+                type: 'quote',
+                priority: 3,
+                template: 'hero-box-templates/quote.html'
+            },
+            {
+                quote: 'The world is having an asymmetric education problem, and you actually have a program where 85% of students get a job.',
+                author: 'Spencer Thompson',
+                credential: 'CEO, Sokanu',
+                imageUrl: 'people/spencer-thompson.jpg',
+                type: 'quote',
+                priority: 5,
                 template: 'hero-box-templates/quote.html'
             }
         ];
@@ -79,23 +92,28 @@ saveINTEapp.controller('heroGridController',
         // Hero Grid Items (vine embeds)
 
         $scope.vineBoxes = [
-            {
-                title: 'Glitch Mob',
-                embedUrl: 'http://vine.co/v/b55LOA1dgJU/embed/simple',
-                type: 'video',
-                priority: 5,
-                template: 'hero-box-templates/vine.html'
-            }
+            // {
+            //     title: 'Glitch Mob',
+            //     embedUrl: 'http://vine.co/v/b55LOA1dgJU/embed/simple',
+            //     type: 'video',
+            //     priority: 5,
+            //     template: 'hero-box-templates/vine.html'
+            // }
         ];
 
         // Hero Grid Items (youtube embeds)
 
         $scope.youtubeBoxes = [
             {
-                title: 'Ride Never Stop',
-                embedUrl: 'http://www.youtube.com/embed/ydM2YqukXgo',
+                embedUrl: 'http://www.youtube.com/embed/NvK1F_odamo',
                 type: 'video',
-                priority: 4,
+                priority: 6,
+                template: 'hero-box-templates/youtube.html'
+            },
+            {
+                embedUrl: 'http://www.youtube.com/embed/kJcPCIAGfeY',
+                type: 'video',
+                priority: 7,
                 template: 'hero-box-templates/youtube.html'
             }
         ];
