17e1d85989c23ab5a953f4bcb84f5207287d650f
Jørgen Hookham <JorgenHookham@Gmail.com>
2013-05-01 23:17:19 -0700

    Update content.

--- app/scripts/controllers/heroGrid.js
+++ app/scripts/controllers/heroGrid.js
@@ -20,71 +20,94 @@ saveINTEapp.controller('heroGridController',
 
         // Hero Grid Items (photos)
 
-        // $scope.photoBoxes = [
-        //     {
-        //         title: 'Save The Future',
-        //         caption: 'The IDL program is too awesome.',
-        //         imageUrl: 'save-the-future.jpg',
-        //         type: 'photo',
-        //         priority: 2,
-        //         template: 'hero-box-templates/photo.html'
-        //     },
-        //     {
-        //         title: 'Back At The Lab',
-        //         caption: 'A Weekend at the familiar AR209 to save the program that helped make us.',
-        //         imageUrl: 'back-at-the-lab.jpg',
-        //         type: 'photo',
-        //         priority: 2,
-        //         template: 'hero-box-templates/photo.html'
-        //     }
-        // ];
-
-        // Hero Grid Items (quote)
-
-        $scope.quoteBoxes = [
+        $scope.photoBoxes = [
             {
                 quote: 'The goal is to put people in the work force and be employable, we look to these programs for our new stars.',
                 author: 'Jason Bailey',
                 credential: 'CEO, East Side Games',
                 imageUrl: 'people/jason-bailey.jpg',
-                type: 'quote',
-                priority: 2,
-                template: 'hero-box-templates/quote.html'
+                type: 'photo',
+                priority: 8,
+                template: 'hero-box-templates/photo.html'
             },
             {
                 quote: 'We have one of the best programs in the university, if anything it should be looked at as one of it\'s crowned jewels.',
                 author: 'Parker Busswood',
                 credential: 'Front-End Engineer, East Side Games',
                 imageUrl: 'people/parker-busswood.jpg',
-                type: 'quote',
-                priority: 1,
-                template: 'hero-box-templates/quote.html'
+                type: 'photo',
+                priority: 2,
+                template: 'hero-box-templates/photo.html'
             },
             {
                 quote: 'People from the INTE program come ready to wear many hats. Cutting this program would leave a big gap.',
                 author: 'Josh Nilson',
                 credential: 'COO, East Side Games',
                 imageUrl: 'people/josh-nilson.jpg',
-                type: 'quote',
-                priority: 4,
-                template: 'hero-box-templates/quote.html'
+                type: 'photo',
+                priority: 8,
+                template: 'hero-box-templates/photo.html'
             },
             {
                 quote: 'As an alumni I stand to lose my biggest network, fresh talent coming out of the program.',
                 author: 'Jørgen Hookham',
                 credential: 'Designer, Sokanu',
                 imageUrl: 'people/jorgen-hookham.jpg',
-                type: 'quote',
-                priority: 3,
-                template: 'hero-box-templates/quote.html'
+                type: 'photo',
+                priority: 5,
+                template: 'hero-box-templates/photo.html'
             },
             {
                 quote: 'The world is having an asymmetric education problem, and you actually have a program where 85% of students get a job.',
                 author: 'Spencer Thompson',
                 credential: 'CEO, Sokanu',
                 imageUrl: 'people/spencer-thompson.jpg',
+                type: 'photo',
+                priority: 6,
+                template: 'hero-box-templates/photo.html'
+            }
+        ];
+
+        // Hero Grid Items (quote)
+
+        $scope.quoteBoxes = [
+            {
+                quote: 'The vast majority of graduates of this program get jobs. Good, long term jobs.',
+                author: 'Dave Rankin',
                 type: 'quote',
-                priority: 5,
+                priority: 1,
+                template: 'hero-box-templates/quote.html'
+            },
+            {
+                quote: 'Coding is the new literacy.',
+                author: 'Hank Leber',
+                credential: 'CEO & Co-Founder GonnaBe',
+                type: 'quote',
+                priority: 1,
+                template: 'hero-box-templates/quote.html'
+            },
+            {
+                quote: 'For the foreseeable future, coding is one of the most important and desirable skills there is.',
+                author: 'Asher Hunt',
+                credential: 'Lead Mobile Designer, LivePerson',
+                type: 'quote',
+                priority: 3,
+                template: 'hero-box-templates/quote.html'
+            },
+            {
+                quote: 'It\'s the battle of the tech literate vs. the tech illiterate.',
+                author: 'Hank Leber',
+                credential: 'CEO & Co-Founder GonnaBe',
+                type: 'quote',
+                priority: 7,
+                template: 'hero-box-templates/quote.html'
+            },
+            {
+                quote: 'Learn to code. You won\'t regret it.',
+                author: 'Hank Leber',
+                credential: 'CEO & Co-Founder GonnaBe',
+                type: 'quote',
+                priority: 3,
                 template: 'hero-box-templates/quote.html'
             }
         ];
@@ -107,13 +130,13 @@ saveINTEapp.controller('heroGridController',
             {
                 embedUrl: 'http://www.youtube.com/embed/NvK1F_odamo',
                 type: 'video',
-                priority: 6,
+                priority: 4,
                 template: 'hero-box-templates/youtube.html'
             },
             {
                 embedUrl: 'http://www.youtube.com/embed/kJcPCIAGfeY',
                 type: 'video',
-                priority: 7,
+                priority: 6,
                 template: 'hero-box-templates/youtube.html'
             }
         ];
