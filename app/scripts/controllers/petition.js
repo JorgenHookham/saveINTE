e7d4373aa435c6429da8f11df606dd7d771f9f7d
Troy Tucker <troynoahtucker@gmail.com>
2013-05-02 23:41:13 -0700

    Connected to Change.org API

--- app/scripts/controllers/petition.js
+++ app/scripts/controllers/petition.js
@@ -5,9 +5,9 @@ saveINTEapp.controller('petitionController',
 
         $scope.signatures = 377;
 
-        var changeApiKey    = 'e10d95d6a68b009a613ddcdc1eda7fbc83c639d1ca662a4003bd6f9972d47a88';
+        var changeApiKey    = '3183c8e44803aa6f8721e6990b1d94423ee6d35b0ca6927ec5f7a2d10fac5e97';
 
-        var requestUrl = 'https://api.change.org/v1/petitions/get_id';
+        var requestUrl = 'https://api.change.org/v1/petitions/1106511/signatures';
         var petitionUrl = 'https://www.change.org/en-CA/petitions/capilano-university-save-the-interactive-design-program';
 
         var requestParameters = {
@@ -22,9 +22,10 @@ saveINTEapp.controller('petitionController',
             data: requestParameters,
             success: function(data) {
                 console.log(data);
-                // $scope.$apply(function(){
-                //     $scope.signatures = data.signatures.length;
-                // });
+                $scope.$apply(function(){
+                    $scope.signatures = data.signature_count;
+                });
+
             }
         });
 
